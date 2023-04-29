254e7822bcc626b227c496ad2747d4d65751a6b4
pawanrijal <pwnrijal@gmail.com>
2023-04-29 18:52:37 +0545

    updated categories

--- seeders/20230102135713-demo-categories.js
+++ seeders/20230102135713-demo-categories.js
@@ -4,57 +4,84 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("categories", [
+      // expense categories
       {
         id: 1,
-        title: "Household Items",
+        title: "Housing (rent/mortgage, utilities, property taxes, repairs)",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         id: 2,
-        title: "House repair/maintenance",
+        title: "Transportation",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         id: 3,
-        title: "Loans/Advances given",
+        title: "Transportation",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         id: 4,
-        title: "Repayment of loans/advances",
+        title: "Entertainment",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         id: 5,
-        title: "Deposit to bank account",
+        title: "Health",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         id: 6,
-        title: "Real Estate Investment",
+        title: "Education",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         id: 7,
-        title: "Vehicle and Machineries",
+        title: "Debt payments (credit card bills, loans)",
         created_at: new Date(),
         updated_at: new Date(),
       },
+
+      // income categories
       {
         id: 8,
-        title: "Food",
+        title: "Salary",
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         id: 9,
-        title: "Others",
+        title: "Investment income (dividends, capital gains, interest)",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: 10,
+        title: "Rental income",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: 11,
+        title: "Gifts/inheritances (unexpected income from family or friends)",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: 12,
+        title: "Retirement income (social security, pensions, annuities)",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: 13,
+        title: "Miscellaneous",
         created_at: new Date(),
         updated_at: new Date(),
       },
