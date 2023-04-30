0b3f94005f93e094a721250f9f421a970e8f36c3
pawanrijal <pwnrijal@gmail.com>
2023-04-30 16:21:02 +0545

    fixed dashboard

--- service/dashboard.service.js
+++ service/dashboard.service.js
@@ -2,18 +2,22 @@ const { transaction, finance } = require("../lib/database.connection");
 
 class DashboardService {
   async getData(user) {
-    const totalIncome = await finance.sum("amount", {
-      where: { type: "income", userId: user.id },
-    });
-    const totalExpense = await finance.sum("amount", {
-      where: { type: "expense", userId: user.id },
-    });
-    const totalCashIn = await transaction.sum("amount", {
-      where: { type: "in", userId: user.id },
-    });
-    const totalCashOut = await transaction.sum("amount", {
-      where: { type: "out", userId: user.id },
-    });
+    const totalIncome =
+      (await finance.sum("amount", {
+        where: { type: "income", userId: user.id },
+      })) ?? 0;
+    const totalExpense =
+      (await finance.sum("amount", {
+        where: { type: "expense", userId: user.id },
+      })) ?? 0;
+    const totalCashIn =
+      (await transaction.sum("amount", {
+        where: { type: "in", userId: user.id },
+      })) ?? 0;
+    const totalCashOut =
+      (await transaction.sum("amount", {
+        where: { type: "out", userId: user.id },
+      })) ?? 0;
 
     const totalBalance =
       totalIncome + totalCashIn - (totalExpense + totalCashOut);
