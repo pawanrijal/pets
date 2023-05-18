d3ed4d6ff1c2d676438d88a146c5d4a62c05e633
pawanrijal <pwnrijal@gmail.com>
2023-05-18 12:28:53 +0545

    algorithm implemented in profile pic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,19 +1,20 @@
 const UserService = require("../service/user.service");
 const successResponse = require("../utils/successResponse");
-const jwt = require("jsonwebtoken");
+const IAes = require("../algorithm/aes");
 
 require("dotenv").config();
 
 class UserController {
   async signup(req, res, next) {
     try {
-      if (req.file) {
-        req.body.profilePic = req.file.filename;
-      }
+      // if (req.file) {
+      //   req.body.profilePic = req.file.filename;
+
+      // }
       const user = await UserService.create(req.body);
       successResponse(res, 200, user, "User Created");
     } catch (err) {
-      console.log(err)
+      console.log(err);
       next(err);
     }
   }
@@ -21,7 +22,12 @@ class UserController {
   async update(req, res, next) {
     try {
       if (req.file) {
-        req.body.profilePic = req.file.path;
+        // Aes.encrypt("big secret", "pāşšŵōřđ", 256);
+        req.body.profilePic = IAes.encrypt(
+          process.env.ENCRYPTION_SECRET,
+          req.file.path,
+          256
+        );
       }
       const userData = await UserService.update(req.body, req.user);
       successResponse(res, 200, userData, "User updated");
@@ -33,7 +39,7 @@ class UserController {
   async login(req, res, next) {
     try {
       const data = await UserService.login(req.body);
-      successResponse(res, 200, data, "Logged in Successfully")
+      successResponse(res, 200, data, "Logged in Successfully");
     } catch (err) {
       console.log(err);
       next(err);
@@ -44,6 +50,8 @@ class UserController {
     try {
       const user = req.user;
       user.password = undefined;
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpires = undefined;
       successResponse(res, 200, user, "User Profile");
     } catch (err) {
       next(err);
@@ -51,53 +59,48 @@ class UserController {
   }
   async changePassword(req, res, next) {
     try {
-      const userData = await UserService.changePassword(req.body)
+      const userData = await UserService.changePassword(req.body);
       successResponse(res, 200, userData, "Password Changed");
-    }
-    catch (err) {
-      next(err)
+    } catch (err) {
+      next(err);
     }
   }
 
   async forgotPassword(req, res, next) {
     try {
-      const response = await UserService.forgotPassword(req.headers.host, req.body)
+      const response = await UserService.forgotPassword(
+        req.headers.host,
+        req.body
+      );
       successResponse(res, 200, response, "Reset Email");
-    }
-    catch (err) {
-      next(err)
+    } catch (err) {
+      next(err);
     }
   }
-
 
   async verifyToken(req, res, next) {
     try {
-      const id=req.params.id;
-      const token=req.params.token
-      req.body.token=token;
-      const response = await UserService.verifyToken(req.body,id);
+      const id = req.params.id;
+      const token = req.params.token;
+      req.body.token = token;
+      const response = await UserService.verifyToken(req.body, id);
       successResponse(res, 200, response, "Valid Url");
-    }
-    catch (err) {
-      next(err)
+    } catch (err) {
+      next(err);
     }
   }
 
-  async resetPassword(req,res,next){
+  async resetPassword(req, res, next) {
     try {
-      const id=req.params.id;
-      const token=req.params.token
-      req.body.token=token;
-      const response = await UserService.resetPassword(req.body,id);
+      const id = req.params.id;
+      const token = req.params.token;
+      req.body.token = token;
+      const response = await UserService.resetPassword(req.body, id);
       successResponse(res, 200, response, "Password changed");
-    }
-    catch (err) {
-      next(err)
+    } catch (err) {
+      next(err);
     }
   }
 }
-
-
-
 
 module.exports = new UserController();
